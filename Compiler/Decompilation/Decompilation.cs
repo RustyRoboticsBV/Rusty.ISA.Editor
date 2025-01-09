810d8e54652fcdca88f97d8b9be01dc4b9c31038
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-01-09 01:18:32 +0100

    Added decompiler folder.

---
+++ Compiler/Decompilation/Decompilation.cs
@@ -0,0 +1,8 @@
+﻿namespace Rusty.CutsceneEditor.Compiler
+{
+    /// <summary>
+    /// A base class for all graph node to inspector decompilers.
+    /// </summary>
+    public abstract class Decompiler
+    { }
+}
